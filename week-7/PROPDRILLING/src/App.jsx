c8054d20b3a4369d192fddbb5d51123327d5f0fc
quanta2k <72522871+quanta2k@users.noreply.github.com>
2024-01-17 14:23:23 +0530

    WEEK-7 Updated

--- week-7/PROPDRILLING/src/App.jsx
+++ week-7/PROPDRILLING/src/App.jsx
@@ -1,26 +1,39 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import {CountContext} from "./context"
 import './App.css'
+import { useContext } from 'react'
 
 function App() {
   const [count, setCount] = useState(0);
-
+  //wrap anyone that wants to use the teleported value inside a provider
   return (
     <>
-      <Count count={count}/>
-      <Buttons count={count} setCount={setCount}/>
+    <CountContext.Provider value={count}>
+      <Count setCount={setCount}/>
+    </CountContext.Provider>
     </>
   )
 }
 
-function Count({count}){
+function Count({setCount}){
+  return <div>
+    <CountRenderer />
+    <Buttons setCount={setCount}/>
+  </div>
+}
+
+
+function CountRenderer(){
+  const count = useContext(CountContext)
   return <div>
     {count}
   </div>
 }
 
-function Buttons({count,setCount}){
+function Buttons({setCount}){
+  const count =useContext(CountContext);
   return<div>
   <button onClick={()=>{
     setCount(count+1)

4d3c7830a17254d03fa0962ae1fa2e73cabcb4e6
quanta2k <72522871+quanta2k@users.noreply.github.com>
2024-02-03 13:56:01 +0530

    typescript prac

--- node-app/src/a.ts
+++ node-app/src/a.ts
@@ -13,3 +13,46 @@ type Manager = {
 };
 
 type TechLead=Employee& Manager;
+
+
+type keyInput= "up" | "down" | "left" | "right"
+
+enum Direction{
+  Up,
+  Down,
+  Left,
+  Right
+}
+function doSomething(keyPressed: Direction){
+  //do something
+  if(keyPressed==Direction.Up){
+
+  }
+}
+
+
+doSomething(Direction.Up)
+doSomething(Direction.Down);
+
+type Input=number| string;
+
+function firstEle(arr: Input[]){
+  return arr[0];  
+}
+
+
+function identity<T>(arg:T):T{
+  return arg;
+}
+
+let output1=identity<String>("Mohak")
+let output2=identity<Number>(100)
+
+output1.toUpperCase();
+
+function getFirstElement<T>(arr: T[]){
+  return arr[0];
+}
+
+const el=getFirstElement(["harkirat","singh"]);
+console.log(el.toLowerCase());

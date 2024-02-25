3c0e2fee024ab2da37faa55157c837fad1ecaf05
quanta2k <72522871+quanta2k@users.noreply.github.com>
2024-02-26 00:55:45 +0530

    updated

---
+++ medium/backend/src/index.ts
@@ -0,0 +1,85 @@
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { Hono } from "hono";
+import { sign, verify } from "hono/jwt";
+
+// Create the main Hono app
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+}>();
+
+app.use("", async (c, next) => {
+  const header = c.req.header("authorization") || "";
+  //Bearer token=>["beader","token"]
+  const token = header.split(" ")[1];
+  const response = await verify(header, c.env.JWT_SECRET);
+  if (response.id) {
+    next();
+  } else {
+    c.status(403);
+    return c.json({
+      error: "unauthorised",
+    });
+  }
+  await next();
+});
+
+app.post("/api/v1/signup", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email: body.email,
+        password: body.password,
+      },
+    });
+    const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+    return c.json({ jwt });
+  } catch (e) {
+    c.status(403);
+    return c.json({ error: "error while signing up" });
+  }
+});
+
+app.post("/api/v1/signin", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+  const user = await prisma.user.findUnique({
+    //@ts-ignore
+    where: {
+      email: body.email,
+    },
+  });
+
+  if (!user) {
+    c.status(403);
+    return c.json({ error: "user not found" });
+  }
+
+  const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
+  return c.json({ jwt });
+});
+
+app.post("/api/v1/blog", (c) => {
+  return c.text("POST /api/v1/blog");
+});
+
+app.put("/api/v1/blog", (c) => {
+  return c.text("PUT /api/v1/blog");
+});
+
+app.get("/api/v1/blog/:id", (c) => {
+  return c.text("GET /api/v1/blog");
+});
+
+export default app;

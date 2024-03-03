36aa9dace0f8b0b88448fcadf1c744d7f322d7e5
quanta2k <72522871+quanta2k@users.noreply.github.com>
2024-03-03 20:09:53 +0530

    Next

---
+++ next-prac14/app/(auth)/signin/page.tsx
@@ -0,0 +1,7 @@
+import { SigninComponent } from "@/components/Signin";
+
+export default function SigninPage() {
+    return <div>
+        <SigninComponent/>
+    </div>
+}
--- next-prac14/app/(auth)/signup/page.tsx
+++ next-prac14/app/(auth)/signup/page.tsx
@@ -1,9 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div>
-       root route
-    </div>
-  );
+  return <div>hi there</div>;
 }

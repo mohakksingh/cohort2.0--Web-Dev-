858b4db4213ac92c14fc18bc201b50deccc9fd09
quanta2k <72522871+quanta2k@users.noreply.github.com>
2024-03-04 11:01:35 +0530

    Commit by Sahil

--- medium/backend/src/index.ts
+++ medium/backend/src/index.ts
@@ -1,7 +1,8 @@
-import { PrismaClient } from "@prisma/client/edge";
-import { withAccelerate } from "@prisma/extension-accelerate";
+
 import { Hono } from "hono";
-import { sign, verify } from "hono/jwt";
+import { userRouter } from "./routes/user";
+import { blogRouter } from "./routes/blogs";
+
 
 // Create the main Hono app
 const app = new Hono<{
@@ -11,78 +12,8 @@ const app = new Hono<{
   };
 }>();
 
-app.use("", async (c, next) => {
-  const header = c.req.header("authorization") || "";
-  //Bearer token=>["beader","token"]
-  const token = header.split(" ")[1];
-  const response = await verify(header, c.env.JWT_SECRET);
-  if (response.id) {
-    next();
-  } else {
-    c.status(403);
-    return c.json({
-      error: "unauthorised",
-    });
-  }
-  await next();
-});
+app.route("/api/v1/user",userRouter)
+app.route("/api/v1/blog",blogRouter)
 
-app.post("/api/v1/user/signup", async (c) => {
-  const prisma = new PrismaClient({
-    datasourceUrl: c.env?.DATABASE_URL,
-  }).$extends(withAccelerate());
-
-  const body = await c.req.json();
-  try {
-    const user = await prisma.user.create({
-      data: {
-        email: body.email,
-        password: body.password,
-      },
-    });
-    const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
-    return c.json({ jwt });
-  } catch (e) {
-    c.status(403);
-    return c.json({ error: "error while signing up" });
-  }
-});
-
-app.post("/api/v1/user/signin", async (c) => {
-  const prisma = new PrismaClient({
-    datasourceUrl: c.env?.DATABASE_URL,
-  }).$extends(withAccelerate());
-
-  const body = await c.req.json();
-  const user = await prisma.user.findUnique({
-    //@ts-ignore
-    where: {
-      email: body.email,
-    },
-  });
-
-  if (!user) {
-    c.status(403);
-    return c.json({ error: "user not found" });
-  }
-
-  const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
-  return c.json({ jwt });
-});
-
-app.post("/api/v1/blog", (c) => {
-  return c.text("POST /api/v1/blog");
-});
-
-app.put("/api/v1/blog", (c) => {
-  return c.text("PUT /api/v1/blog");
-});
-
-app.get("/api/v1/blog/:id", (c) => {
-  return c.text("GET /api/v1/blog");
-});
-app.get("/api/v1/blog/bulk", (c) => {
-  return c.text("GET /api/v1/blog");
-});
 
 export default app;

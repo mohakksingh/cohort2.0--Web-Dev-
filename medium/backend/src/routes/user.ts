790492564b5c3ab06a644dd5634eda5d14945da5
quanta2k <72522871+quanta2k@users.noreply.github.com>
2024-03-05 19:27:27 +0530

    updated

--- medium/backend/src/routes/user.ts
+++ medium/backend/src/routes/user.ts
@@ -11,33 +11,33 @@ export const userRouter = new Hono<{
   };
 }>();
 
-userRouter.post("/signup", async (c) => {
+userRouter.post('/signup', async (c) => {
   const body = await c.req.json();
   const { success } = signupInput.safeParse(body);
   const prisma = new PrismaClient({
-    datasourceUrl: c.env?.DATABASE_URL,
-  }).$extends(withAccelerate());
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
 
   try {
     const user = await prisma.user.create({
       data: {
-        email: body.email,
+        email: body.username,
         password: body.password,
-        name: body.name,
-      },
-    });
-    const jwt = await sign(
-      {
-        id: user.id,
-      },
-      c.env.JWT_SECRET
-    );
-    return c.text(jwt);
-  } catch (e) {
+        name: body.name
+      }
+    })
+    const jwt = await sign({
+      id: user.id
+    }, c.env.JWT_SECRET);
+
+    return c.text(jwt)
+  } catch(e) {
+    console.log(e);
     c.status(411);
-    return c.text("Invalid");
+    return c.text('Invalid')
   }
-});
+})
+
 
 userRouter.post("/signin", async (c) => {
   const prisma = new PrismaClient({

b61e97524f18f0906fb56e02939ff2450734e3a9
quanta2k <72522871+quanta2k@users.noreply.github.com>
2024-05-05 20:36:53 +0530

    backend in nextjs

---
+++ nextapp-backend/app/api/user/route.ts
@@ -0,0 +1,9 @@
+
+export function GET(){
+    //database logic
+    return Response.json({
+        email:"mohak@gmail.com",
+        name:"Mohak"
+    })
+}
+
--- nextapp/app/(auth)/signup/page.tsx
+++ nextapp/app/(auth)/signup/page.tsx
@@ -1,7 +1,8 @@
+import { Signup } from "@/components/Signup";
 import Image from "next/image";
 
 export default function() {
   return (
-    <div>Signup</div>
+    <Signup/>
   );
 }

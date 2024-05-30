1c01d0c2524553d6a2b781b3b9842da338c5bdc3
quanta2k <72522871+quanta2k@users.noreply.github.com>
2024-05-31 01:52:53 +0530

    learnt next-auth

---
+++ week-16-auth/app/signup/page.tsx
@@ -0,0 +1,9 @@
+export default function(){
+    return <div>
+        signup page
+        <input placeholder="username" type="text" />
+        <input placeholder="password" type="text" />
+       <button>Submit</button>
+        
+    </div>
+}
